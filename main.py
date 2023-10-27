5d73cd576895788324e5580e8467034845e8ebbd
Malik Smith <smithmalik2468@gmail.com>
2023-10-27 08:27:48 +0000

    cbfs class constructor initialized

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ class cbfs(param.Parameterized):
     db_query=param.String("")
     db_response=param.List([])
     
+    def __init__(self, **params):
+        super(cbfs, self).__init__(**params)
+        self.panels=[]
+        self.loaded_file="file/dir.pdf"
+        self.qa=load_db(self.loaded_file,"stuff",4)
