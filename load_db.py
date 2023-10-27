00cf79ac1316d57637590420375e0be4c2c6c67f
Malik Smith <smithmalik2468@gmail.com>
2023-10-27 04:07:40 +0000

    load db part 1 completed

--- load_db.py
+++ load_db.py
@@ -1,9 +1,19 @@
 import lib
 # def func(file, kwargs={})
-def load_db(file):
+def load_db(file, chain_type, k):
 # Load Documents
     loader = lib.PyPDFLoader(file)
-# Store documents in Vector store
+    documents = loader.load() # Original documents
+    # Define Text Splitter
+    text_splitter = lib.RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=150)
+    # Split documents
+    docs = text_splitter.split_documents(documents) # New Split Documents
 
-#Retrieve Documents
+    # Create embeddings before creating vector store
+    embeddings = lib.OpenAIEmbeddings()
+    # Create Vector Store with embeddings for Semantic Search
+    db = lib.DocArrayInMemorySearch.from_documents(docs = docs,embeddings=embeddings)
 
+    # Create retriever
+    retriever = db.as_retriever(search_type="similarity", search_kwargs={"k":k})
+   
