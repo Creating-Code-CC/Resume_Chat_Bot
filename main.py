4ebbcd4fb21dfcd46e551a4aacd28c932f6c278e
Malik Smith <smithmalik2468@gmail.com>
2023-10-27 08:46:17 +0000

    call_load_db initialized

--- main.py
+++ main.py
@@ -13,3 +13,17 @@ class cbfs(param.Parameterized):
         self.panels=[]
         self.loaded_file="file/dir.pdf"
         self.qa=load_db(self.loaded_file,"stuff",4)
+    def call_load_db(self, count):
+        if count==0 or file_input.value is None:#init or no file specified
+            return pn.pane.Markdown(f"Loaded File: {self.loaded_file}")
+        else:
+            file_input.save("temp.pdf") #local copy
+            self.loaded_file=file_input.file_name
+            button_load.button_style="outline"
+            self.qa=load_db("temp.pdf", "stuff", 4)
+            button_load.button_style="solid"
+            
+        self.clr_history()
+
+        return pn.pane.Markdown(f"Loaded File: {self.loaded_file}")
+    
