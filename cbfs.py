b8516695591e47a5320095a41fae3b2233e8000f
smith <smithmalik@gmail.com>
2023-11-02 12:08:23 -0400

    cbfs syntax bugs updated

--- cbfs.py
+++ cbfs.py
@@ -18,30 +18,26 @@ class cbfs(param.Parameterized):
             return pn.pane.Markdown(f"Loaded File: {self.loaded_file}")
         else:
             file_input.save("temp.pdf") #local copy
-            self.loaded_file=file_input.file_name
+            self.loaded_file=file_input.filename
             button_load.button_style="outline"
             self.qa=load_db("temp.pdf", "stuff", 4)
             button_load.button_style="solid"
-
         self.clr_history()
-
         return pn.pane.Markdown(f"Loaded File: {self.loaded_file}")
+    
     def convchain(self, query):
         if not query:
-            return pn.WidgetBox(pn.Row('User: ', pn.pane.Markdown("", width=600)), scroll=True)
+            return pn.WidgetBox(pn.Row('User:', pn.pane.Markdown("", width=600)), scroll=True)
         result=self.qa({"question": query, "chat_history": self.chat_history})
-        self.chat_history.extend([query, result["answer"]])
+        self.chat_history.extend([(query, result["answer"])])
         self.db_query=result["generated_question"]
         self.db_response=result["source_documents"]
         self.answer=result['answer']
         self.panels.extend([
             pn.Row('User:', pn.pane.Markdown(query, width=600)),
-            pn.Row('ChatBot:', pn.pane.Markdown(self.answer, width=600, styles={"background-color":"#F6F6F6"}))
-
+            pn.Row('ChatBot:', pn.pane.Markdown(self.answer, width=600, styles={'background-color':'#F6F6F6'}))
         ])
-
         inp.value=''#clears loading indicator when cleared.
-
         return pn.WidgetBox(*self.panels,scroll=True)
     
     @param.depends('db_query', )
@@ -55,6 +51,7 @@ class cbfs(param.Parameterized):
             pn.Row(pn.pane.Markdown(f"DB query:", styles={'background-color':'#F6F6F6'})),
             pn.pane.Str(self.db_query)
         )
+        
     @param.depends('db_response', )
     def get_sources(self):
         if not self.db_response:
@@ -63,6 +60,7 @@ class cbfs(param.Parameterized):
         for doc in self.db_response:
             rlist.append(pn.Row(pn.pane.Str(doc)))
         return pn.WidgetBox(*rlist, width=600, scroll=True)
+    
     @param.depends('convchain', 'clr_history')
     def get_chats(self):
         if not self.chat_history:
